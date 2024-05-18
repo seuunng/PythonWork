a69ca7f62da7f1e11755ffc413dc3877c6ae4c7a
seunghee <mnb2856@gmail.com>
2024-05-18 20:17:45 +0900

    5/18 파이선 기초문법 연습

---
+++ 기본연산자.py
@@ -0,0 +1,16 @@
+result = 2 ** 4
+print(result)   # 16
+result = 8 / 5
+print(result)  # 몫: 1.6
+result = 8 // 5
+print(result)  # 몫: 1
+result = 8 % 5
+print(result)  # 나머지: 3
+
+for i in range(100):
+    if i % 11 == 0:
+        print(i)
+print("---------------")
+
+for i in range(5):
+    print(i)
